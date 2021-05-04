fda3c1d9434d9eb6b9638f8c17ab4e8e02c46d70
Muhammad Zamroni <halo@matriphe.com>
2021-05-04 21:29:06 +0200

    Update config

--- config/bendera.php
+++ config/bendera.php
@@ -5,10 +5,9 @@ return [
     /**
      * Set an alias for country. By default, 'UK' and 'EN' will be mapped to 'GB'.
      */
-
     'aliases' => [
         'EN' => 'GB',
         'UK' => 'GB',
-    ]
+    ],
 
 ];
