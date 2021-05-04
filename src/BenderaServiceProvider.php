23d25cf4640ade5eb7d95970e446a614b8ffd003
Muhammad Zamroni <halo@matriphe.com>
2021-05-04 15:21:59 +0200

    Add Bendera config and service provider

---
+++ src/BenderaServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Matriphe\Bendera;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+use Illuminate\Foundation\Application as LaravelApplication;
+use Stidges\CountryFlags\CountryFlag;
+
+class BenderaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->configure();
+        $this->offerPublishing();
+        $this->registerSingletons();
+    }
+
+    /**
+     * Setup the configuration.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/bendera.php', 'bendera');
+    }
+
+    /**
+     * Setup the resource publishing groups.
+     *
+     * @return void
+     */
+    protected function offerPublishing()
+    {
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/bendera.php' => config_path('bendera.php'),
+            ], 'bendera');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('bendera');
+        }
+    }
+
+    /**
+     * Register the singletons in the container.
+     *
+     * @return void
+     */
+    protected function registerSingletons()
+    {
+        $this->app->singleton(BenderaInterface::class, static function () {
+            return new Bendera(new CountryFlag(config('bendera.aliases')));
+        });
+    }
+}
